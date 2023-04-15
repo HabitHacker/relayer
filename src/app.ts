606024d7e4d72af097f0d55dde6f5cee565f4ab7
mi-gongan <kib0202@naver.com>
2023-04-15 21:53:06 +0900

    FIX:route frame

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import config from "./config";
 import cors from "cors";
 import morgan from "morgan";
 import RelayerWallet from "./libs/relayerWallet";
+import { NetworkList, VerifyUnit } from "./types/type";
 
 const app = express();
 
@@ -21,11 +22,48 @@ app.use(morgan("combined"));
 app.post("/habit/setting", async (req, res) => {
   const { habitId, habitInfo, collectionInfo, network } = req.body;
   const _relayerWallet = new RelayerWallet();
-  await _relayerWallet.setHabit(habitId, habitInfo, collectionInfo, network);
+  try {
+    await _relayerWallet.setHabit(habitId, habitInfo, collectionInfo, network);
+  } catch {
+    return res.status(500).json({ message: "fail" });
+  }
+  return res.status(200).json({ message: "success" });
 });
 
-app.post("/habit/verify", (req, res) => {});
+app.post("/habit/verify", async (req, res) => {
+  const {
+    _moderatorAddress,
+    _verifyBundle,
+    _network,
+  }: {
+    _moderatorAddress: string;
+    _verifyBundle: VerifyUnit[];
+    _network: NetworkList;
+  } = req.body;
+  const _relayerWallet = new RelayerWallet();
+  try {
+    await _relayerWallet.verifyBundle(
+      _moderatorAddress,
+      _verifyBundle,
+      _network
+    );
+  } catch {
+    res.status(500).json({ message: "fail" });
+  }
+  return res.status(200).json({ message: "success" });
+});
 
-app.post("/habit/settle", (req, res) => {});
+app.post("/habit/settle", async (req, res) => {
+  const { habitId, network } = req.body;
+  const _relayerWallet = new RelayerWallet();
+  try {
+    await _relayerWallet.settle(habitId, network);
+  } catch {
+    return res.status(500).json({ message: "fail" });
+  }
+  return res.status(200).json({ message: "success" });
+});
 
-app.listen(config.host, () => {});
+app.listen(config.host, () => {
+  console.log("server is running on port " + config.host.port);
+});
